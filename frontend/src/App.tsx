0163a0eea652b996adb0af5e968fd5d3c2e2459a
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2023-12-08 00:53:02 +0900

    add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './component/layout/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div >
-        <button onClick={() => setCount((count) => count + 1)} className='btn btn-primary'>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1 className="text-5xl">
-      Hello world!
-    </h1>
-    </>
+    <header><Header/></header>
+    
   )
 }
 
