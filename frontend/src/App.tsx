4c58cb6b4982dc77ef39080ae2959fbff542bbaa
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2024-02-12 00:39:37 +0900

    修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { Routers } from "./routers/Routers";
 import { Nav } from "./components/layout/Nav";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="pt-10">
