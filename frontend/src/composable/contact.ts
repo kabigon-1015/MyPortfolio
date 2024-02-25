92514a59af460abf7756cf65f815baab6cfb8a3b
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2024-02-26 00:11:33 +0900

    バリデーションの実装

--- frontend/src/composable/contact.ts
+++ frontend/src/composable/contact.ts
@@ -3,6 +3,11 @@ import { init, send } from "@emailjs/browser";
 const USER_ID = "mohaHhCmlcuRx7UeB";
 const SERVICE_ID = "myportfolio-service";
 const TEMPLATE_ID = "myportfolio-template";
+export type ResponseMessage = {
+  isError: boolean;
+  message: string;
+};
+
 export const submit = async (name: string, mail: string, content: string) => {
   if (USER_ID && SERVICE_ID && TEMPLATE_ID) {
     init(USER_ID);
@@ -13,9 +18,60 @@ export const submit = async (name: string, mail: string, content: string) => {
     };
     try {
       await send(SERVICE_ID, TEMPLATE_ID, params);
-      return "送信しました!";
+      const response: ResponseMessage = {
+        isError: false,
+        message: "送信しました!",
+      };
+      return response;
     } catch (error) {
-      return "送信できませんでした。";
+      const response: ResponseMessage = {
+        isError: true,
+        message: "送信できませんでした。",
+      };
+      return response;
     }
   }
+};
+
+export const inVaildText = (text: string) => {
+  const response: ResponseMessage =
+    text.trim().length === 0
+      ? {
+          isError: true,
+          message: "すべての項目を入力してください",
+        }
+      : {
+          isError: false,
+          message: "",
+        };
+  return response;
+};
+
+export const inVaildMail = (mail: string) => {
+  const response: ResponseMessage = !mail.match(/.+@.+\..+/)
+    ? {
+        isError: true,
+        message: "正しいメールアドレスを入力してください",
+      }
+    : {
+        isError: false,
+        message: "",
+      };
+  return response;
+};
+
+export const vaildForm = (name: string, mail: string, content: string) => {
+  if (inVaildText(name).isError) {
+    return inVaildText(name);
+  }
+  if (inVaildText(mail).isError) {
+    return inVaildText(mail);
+  }
+  if (inVaildText(content).isError) {
+    return inVaildText(content);
+  }
+  if (inVaildMail(mail).isError) {
+    return inVaildMail(mail);
+  }
+  return false;
 };
--- frontend/src/routers/Contact.tsx
+++ frontend/src/routers/Contact.tsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
-import { submit } from "../composable/contact";
+import { useRef, useState } from "react";
+import {
+  ResponseMessage,
+  inVaildMail,
+  inVaildText,
+  submit,
+  vaildForm,
+} from "../composable/contact";
+import { NowLoading } from "../components/common/NowLoading";
+import { AlertSuccess } from "../components/common/AlertSuccess";
+import { AlertError } from "../components/common/AlertError";
 
 export const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [content, setContent] = useState("");
-  const submitContact = () => {
-    const massage = submit(name, email, content);
-    console.log(massage);
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [responseMessage, setResponseMessage] = useState<ResponseMessage>();
+  const submitContact = async () => {
+    setIsLoading(true);
+    const errorMassageFromVaild = vaildForm(name, email, content);
+    if (errorMassageFromVaild !== false) {
+      setResponseMessage(errorMassageFromVaild);
+      setIsLoading(false);
+      return false;
+    }
+    console.log(responseMessage?.isError);
+    const message = await submit(name, email, content);
+    setIsLoading(false);
+    message && setResponseMessage(message);
   };
   return (
     <>
@@ -15,44 +35,59 @@ export const Contact = () => {
         ご意見・お問い合わせなど
       </h1>
       <div className="rounded-xl p-10 relative bg-orange-50">
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">お名前</div>
-          <div className="md:w-2/3">
-            <input
-              type="text"
-              placeholder="xxxx"
-              className="input input-bordered w-full max-w-xs"
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">メールアドレス</div>
-          <div className="md:w-2/3">
-            <input
-              type="text"
-              placeholder="xxxx@examle.com"
-              className="input input-bordered w-full max-w-xs"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">内容</div>
-          <div className="md:w-2/3">
-            <textarea
-              className="textarea textarea-bordered h-48 w-96"
-              placeholder="ご意見やお問い合わせをご記入ください"
-              onChange={(e) => setContent(e.target.value)}
-            ></textarea>
-          </div>
-        </div>
-        <button
-          className="btn btn-wide btn-accent"
-          onClick={() => submitContact()}
-        >
-          送信
-        </button>
+        {responseMessage && !isLoading ? (
+          responseMessage.isError ? (
+            <AlertError message={responseMessage.message}></AlertError>
+          ) : (
+            <AlertSuccess message={responseMessage.message}></AlertSuccess>
+          )
+        ) : (
+          <></>
+        )}
+        {isLoading ? (
+          <NowLoading loadingWords="ご意見・お問い合わせ内容を送信しています"></NowLoading>
+        ) : (
+          <>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">お名前</div>
+              <div className="md:w-2/3">
+                <input
+                  type="text"
+                  placeholder="xxxx"
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">メールアドレス</div>
+              <div className="md:w-2/3">
+                <input
+                  type="text"
+                  placeholder="xxxx@examle.com"
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">内容</div>
+              <div className="md:w-2/3">
+                <textarea
+                  className="textarea textarea-bordered h-48 w-96"
+                  placeholder="ご意見やお問い合わせをご記入ください"
+                  onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+              </div>
+            </div>
+            <button
+              className="btn btn-wide btn-accent"
+              onClick={() => submitContact()}
+            >
+              送信
+            </button>
+          </>
+        )}
       </div>
     </>
   );
