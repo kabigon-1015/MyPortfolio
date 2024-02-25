b3933925fc49a45fbe9fe7e75a3387c7cdb27393
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2024-02-25 10:49:36 +0900

    API使えるようにした

---
+++ frontend/src/components/experiences/DisplayExperienceIcon.tsx
@@ -0,0 +1,33 @@
+import { FaRegBuilding } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { HiAcademicCap } from "react-icons/hi";
+import { VscAccount } from "react-icons/vsc";
+import { encodeExperienceType } from "../../models/experiences";
+
+interface Props {
+  experienceType: string;
+}
+
+const DynamicComponent: React.FC<Props> = ({ experienceType }) => {
+  const components: { [key: string]: React.FC } = {
+    FaRegBuilding,
+    FaLaptopCode,
+    HiAcademicCap,
+    VscAccount,
+  };
+
+  const SelectedComponent = components[encodeExperienceType(experienceType)];
+  return <SelectedComponent />;
+};
+
+export const DisplayExperienceIcon = ({
+  experienceType,
+}: {
+  experienceType: string;
+}) => {
+  return (
+    <span className="absolute z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-blue-100">
+      <DynamicComponent experienceType={experienceType} />
+    </span>
+  );
+};
--- frontend/src/components/layout/Nav.tsx
+++ frontend/src/components/layout/Nav.tsx
@@ -1,4 +1,7 @@
 export const Nav = () => {
+  const getImage = (): string => {
+    return new URL(`../../assets/myicon.png`, import.meta.url).href;
+  };
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -7,9 +10,8 @@ export const Nav = () => {
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
+            src={new URL(`../../assets/myicon.png`, import.meta.url).href}
+            className="h-10"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Kentaro Sakabe
