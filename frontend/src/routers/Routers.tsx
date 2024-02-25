1ce4e3da9360844da3d9654552a7480a853b1ed4
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2024-02-26 02:13:53 +0900

    UI配置

--- frontend/src/routers/Routers.tsx
+++ frontend/src/routers/Routers.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./Home";
 import { Experiences } from "./Experiences";
 import { Contact } from "./Contact";
+import { Products } from "./Products";
 
 export const Routers = () => {
   return (
@@ -9,6 +10,7 @@ export const Routers = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/experiences" element={<Experiences />}></Route>
+        <Route path="/products" element={<Products></Products>}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
