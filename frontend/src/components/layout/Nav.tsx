8c12437d14d5312375cca8b6571be66e19a1a4a3
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2024-03-02 01:26:16 +0900

    できた

--- frontend/src/components/layout/Nav.tsx
+++ frontend/src/components/layout/Nav.tsx
@@ -2,10 +2,7 @@ export const Nav = () => {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src={new URL(`../../assets/myicon.png`, import.meta.url).href}
             className="h-10"
--- frontend/src/routers/Home.tsx
+++ frontend/src/routers/Home.tsx
@@ -1,16 +1,90 @@
+import { useEffect, useState } from "react";
+import { Tech } from "../models/tech";
+import { client } from "../composable/experiences";
+import { UsingTechBadge } from "../components/products/UsingTechBadge";
+import { IconContext } from "react-icons";
+import { FaGithub } from "react-icons/fa";
+import { BiMailSend } from "react-icons/bi";
+
+const offset = 0;
+const limit = 20;
 export const Home = () => {
+  const heroUrl = new URL(`../assets/hero.jpg`, import.meta.url).href;
+  const githubUrl = "https://github.com/kabigon-1015";
+  const [techData, setTechData] = useState<Tech[]>();
+  useEffect(() => {
+    (async () => {
+      const tech = await client
+        .get<any>({
+          endpoint: "tech",
+          queries: {
+            limit,
+            offset,
+          },
+        })
+        .catch((err) => {
+          throw Error(`Reponse failed: ${err}`);
+        });
+      setTechData(tech.contents);
+    })();
+  }, []);
   return (
-    <div className="grid flex-grow h-44 card rounded-box place-items-center">
-      <div className="mockup-code">
-        <pre data-prefix="$">
-          <code>npm i daisyui</code>
-        </pre>
-        <pre data-prefix=">" className="text-warning">
-          <code>installing...</code>
-        </pre>
-        <pre data-prefix=">" className="text-success">
-          <code>Done!</code>
-        </pre>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={heroUrl} className="max-w-sm rounded-lg shadow-2xl ml-5" />
+        <div>
+          <h1 className="text-5xl font-bold">Welcome!</h1>
+          <p className="py-6">
+            大阪に住んでいる修士1年です。エンジニアを目指して活動しています。
+          </p>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">名前：</div>
+            <div className="md:w-3/5">
+              <p>酒部健太郎（さかべけんたろう）</p>
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">所属：</div>
+            <div className="md:w-3/5">
+              <p>同志社大学大学院 理工学研究科 情報工学専攻</p>
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">研究テーマ：</div>
+            <div className="md:w-3/5">
+              <p>
+                Generating Interior Images with Latent User Preferences through
+                GANs
+              </p>
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">主なスキル：</div>
+            <div className="md:w-3/5">
+              {techData &&
+                techData.map((tech: Tech) => {
+                  return (
+                    <UsingTechBadge tech={tech} key={tech.id}></UsingTechBadge>
+                  );
+                })}
+              <br></br>．．．など
+            </div>
+          </div>
+          <div className="ml-80 flex">
+            <IconContext.Provider value={{ size: "50px" }}>
+              <div className="p-2">
+                <a href={githubUrl}>
+                  <FaGithub></FaGithub>
+                </a>
+              </div>
+              <div className="p-2">
+                <a href="/contact">
+                  <BiMailSend></BiMailSend>
+                </a>
+              </div>
+            </IconContext.Provider>
+          </div>
+        </div>
       </div>
     </div>
   );
